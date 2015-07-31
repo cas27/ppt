c76076059531f8a9716f6df0422151af94124261
Cory Schmitt <cory@schmitty.me>
2015-07-31 10:02:01 -0700

    Syntax dev config

--- config/dev.exs
+++ config/dev.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
 config :ppt,
-user: "AeosmXJ1nZWwJgErHHhzxpOS0fH7Ki6qu4LVEotJA3al_bsZcnnYsvc1dDAi_71JYIdwkxeFHDu4sdEy"
+user: "AeosmXJ1nZWwJgErHHhzxpOS0fH7Ki6qu4LVEotJA3al_bsZcnnYsvc1dDAi_71JYIdwkxeFHDu4sdEy",
 secret: "EDL8FGl7O6Xy-oIjdvcl4TRrx9EqKEkU_vKTi2qV3S95vfh1RB9gqiBJ6Uys-NuhSWCOn3FO84JFdpXy",
 currency: "USD"
